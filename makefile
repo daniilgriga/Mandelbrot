7a4d7b2b056a59050abcc85e7b308f4c248363ba
daniilgriga <daniilextra13@gmail.com>
2025-03-27 03:07:24 +0300

    added getopt

--- makefile
+++ makefile
@@ -12,7 +12,7 @@ FLAGS = -D _DEBUG -ggdb3  -O0 -Wall -Wextra  -Waggressive-loop-optimizations \
 		-Wvariadic-macros -Wno-missing-field-initializers -Wno-narrowing \
 		-Wno-varargs -Wstack-protector \
 		-fstack-protector -fstrict-overflow -flto-odr-type-merging -fno-omit-frame-pointer \
-		-Wlarger-than=8192 -Wstack-usage=8192 -pie -fPIE -Werror=vla \
+		-pie -fPIE -Werror=vla \
 		-fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,$\
 		integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,$\
 		returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr
--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,84 @@
 #include <stdio.h>
 #include <string.h>
+#include <getopt.h>
 #include <SFML/Graphics.hpp>
 
 #include "mandelbrot_alg.hpp"
 
-int main (int argc, const char* argv[])
+static struct option long_options[] =
 {
-    if (argc != 2)
-    {
-        fprintf (stderr, "Usage:\n%s --run=<number of iterations> [without grpahics]\nor\n%s --graphics [for graphics]\n", argv[0], argv[0]);
-        return 1;
-    }
+    {"graphics",       no_argument, NULL, 'g'},
+    {  "run"   , required_argument, NULL, 'r'},
+    {   NULL   ,         0        , NULL,  0 }
+};
 
-    bool useGraphics = (argc == 2 && strcmp(argv[1], "--graphics") == 0);
+int GraphicsPart (void);
+
+int main (int argc, char* argv[]) // REVIEW
+{
+    bool useGraphics = false;
+    int iterations = 0;
+    int option = 0;
+
+    while ((option = getopt_long (argc, argv, "gr:", long_options, NULL)) != -1)
+    {
+        switch (option)
+        {
+            case 'g':
+                useGraphics = true;
+                break;
+
+            case 'r':
+                iterations = atoi (optarg);
+                break;
+
+            case '?':
+                fprintf (stderr, "Usage:\n%s [-g|--graphics]\nor\n%s [-r|--run]=<number>\n", argv[0], argv[0]);
+                return 1;
+
+            default:
+                fprintf (stderr, "Error: Unexpected option processing error\n");
+        }
+    }
 
     if (useGraphics)
-    {
-        sf::RenderWindow window (sf::VideoMode (800, 800), "Mandelbrot");
-
-        sf::Image image;
-        image.create (800, 800, sf::Color::Black);
-
-        RunMandelbrot (&image, true);
-
-        sf::Texture texture;
-        texture.loadFromImage (image);
-
-        sf::Sprite sprite (texture);
-
-        while (window.isOpen())
-        {
-            sf::Event event;
-            while (window.pollEvent (event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close ();
-            }
-
-            window.clear ();
-            window.draw (sprite);
-            window.display ();
-        }
-    }
+        GraphicsPart ();       // if i add keyboard handler -> needs a error handler
     else
     {
-        if (strncmp (argv[1], "--run=", 6) != 0)
-        {
-            fprintf (stderr, "Error: Argument must be in the form --run=<number>\n");
-            return 1;
-        }
-
-        const char* number_str = argv[1] + 6;
-        char* endptr;
-        long iterations = strtol (number_str, &endptr, 10);
-
-        if (*endptr != '\0' || number_str == endptr)
-        {
-            fprintf(stderr, "Error: Invalid number in --run argument\n");
-            return 1;
-        }
-
-        if (iterations < 0)
-        {
-            fprintf(stderr, "Error: Number of iterations must be non-negative\n");
-            return 1;
-        }
-
         for (int i = 0; i < iterations; i++)
             RunMandelbrot (nullptr, false);
     }
+
+    return 0;
+}
+
+int GraphicsPart (void)
+{
+    sf::RenderWindow window (sf::VideoMode (800, 800), "Mandelbrot");
+
+    sf::Image image;
+    image.create (800, 800, sf::Color::Black);
+
+    RunMandelbrot (&image, true);
+
+    sf::Texture texture;
+    texture.loadFromImage (image);
+
+    sf::Sprite sprite (texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent (event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close ();
+        }
+
+        window.clear ();
+        window.draw (sprite);
+        window.display ();
+    }
+
     return 0;
 }
